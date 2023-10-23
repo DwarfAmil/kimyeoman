c77cfb18b7d4632b823294141cd530454dd193ac
kimyoojun <anano3134@gmail.com>
2023-10-23 16:27:30 +0900

    test

--- js_test.js
+++ js_test.js
@@ -1,2 +1,7 @@
-console.log(document.getElementById("text"))
-console.log(document.getElementById("p"))
+function noReturn(){
+    console.log("반환하지 않는다, 아무것도!")
+}
+
+
+const result = noReturn()
+console.log(result)  
