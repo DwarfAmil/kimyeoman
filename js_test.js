39c2df2481d7589086f3accd441cf72d63d21c23
kimyoojun <anano3134@gmail.com>
2023-10-25 16:28:54 +0900

    test

--- js_test.js
+++ js_test.js
@@ -1,7 +1,12 @@
-function noReturn(){
-    console.log("반환하지 않는다, 아무것도!")
+const btn1 = document.getElementById("one")
+const btn2 = document.getElementById("two")
+const btn3 = document.getElementById("three")
+
+const handleClick = function(){
+    console.log("저를 클릭하셨나요?")
 }
 
-
-const result = noReturn()
-console.log(result)  
+btn2.addEventListener('click', handleClick)
+btn2.addEventListener('click', function(){
+    console.log("또 다른 핸들러가 추가 등록되었다!")
+})
